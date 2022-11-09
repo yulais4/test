c3d6662d4da3a846ae79779789151b57ab76ad98
yulais4 <113600258+yulais4@users.noreply.github.com>
2022-11-09 23:37:37 +0300

    Update web-app-1.php

--- web-app-1.php
+++ web-app-1.php
@@ -2,7 +2,7 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
 
-echo "<h1>Hello from web server</h1><br>";
+echo "<h1>Hello from web-app N1</h1><br>";
 echo "Server IP Address is: $ip_server", "<br><p>";
 echo "Made by <front color=blue>Yuriy Vasilev";
 

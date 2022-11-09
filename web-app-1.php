dc91a4e0bfecda5f186cb1fcbcfd04b3e05f3a49
yulais4 <113600258+yulais4@users.noreply.github.com>
2022-11-09 23:36:55 +0300

    Create web-app-1.php

---
+++ web-app-1.php
@@ -0,0 +1,9 @@
+
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from web server</h1><br>";
+echo "Server IP Address is: $ip_server", "<br><p>";
+echo "Made by <front color=blue>Yuriy Vasilev";
+
+?>
